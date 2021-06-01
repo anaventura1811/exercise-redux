f1b2336d0724196bc593c07f3dd29d9d93ff7a20
rogerio <rogerio.cl@gmail.com>
2021-06-01 18:25:20 -0300

    implementaçao do formulario de castro de clientes

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Cadastro() {
-  return (
-    <div>
-      
-    </div>
+
+class Cadastro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      client: {
+        name: '',
+        age: '',
+        email: '',
+      },
+    }
+
+  };
+  handleClientInformation = ( { target }) => {
+    this.setState({ client: { [ target.id ]: target.value } });
+  };
+  resgisterClient = (event) => {
+    event.preventDefault();
+    const { client } = this.state;
+    //chama a função que faz a escrita no store do redux
+  };
+  render() {
+    const { name, age, email } = this.state;
+    return (
+    <form>
+      <label htmlFor="name">
+        Nome:
+        <input type="text" id="name" value={ name } onChange={ this.handleClientInformation } />
+      </label>
+      <label htmlFor="age">
+        Idade:
+        <input type="text" id="age" value={ age } onChange={ this.handleClientInformation } />
+      </label>
+      <label htmlFor="email">
+        E-mail:
+        <input type="email" name="email" id="email" value={ email } onChange={ this.handleClientInformation } />
+      </label>
+      <button type="submit" onClick={this.resgisterClient}>Casdastrar</button>
+    </form>
   )
+  }  
 }
+
+export default Cadastro;
+
+import React, { Component } from 'react';
+
+
+class Cadastro extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+Cadastro.propTypes = {
+
+};
 
 export default Cadastro;
